c5dd5f63242ebecb9ed226b2620e726db0a4da5a
priy120 <priyanshu2001pal@gmail.com>
2026-09-19 22:35:24 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package me:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'screens/login_screen.dart';
