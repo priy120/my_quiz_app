16305948139eea0cff43a1ab64fbc787475f10e8
priy120 <priyanshu2001pal@gmail.com>
2026-09-20 12:28:20 +0530

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package0google_fonts/google_fonts.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
