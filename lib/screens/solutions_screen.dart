c9097725b8f87f952d2e38bc413ae50b61ae1cf0
priy120 <priyanshu2001pal@gmail.com>
2026-09-22 10:17:35 +0530

    Update solutions_screen.dart

--- lib/screens/solutions_screen.dart
+++ lib/screens/solutions_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package01/google_fonts/google_fonts.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SolutionsScreen extends StatefulWidget {
