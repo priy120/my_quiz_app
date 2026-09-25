97801dd19088339163d0514c566ce1fade39cd99
priy120 <priyanshu2001pal@gmail.com>
2026-09-25 12:46:12 +0530

    Update quiz_engine_screen.dart

--- lib/screens/quiz_engine_screen.dart
+++ lib/screens/quiz_engine_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_math_fork/flutter_math.dart';
+import 'package:katex_flutter/katex_flutter.dart';
 import 'analysis_screen.dart';
 
 enum QuestionStatus { notVisited, notAnswered, answered, markedForReview, markedAndAnswered }
@@ -157,35 +157,16 @@ class _QuizEngineScreenState extends State<QuizEngineScreen> {
   }
 
   Widget _buildMathOrText(String content, {TextStyle? style}) {
-    String cleanContent = content
+    String formattedContent = content
         .replaceAll(r'\[', r'$')
         .replaceAll(r'\]', r'$')
         .replaceAll(r'\(', r'$')
         .replaceAll(r'\)', r'$');
 
-    if (cleanContent.contains(r'$')) {
-      List<Widget> spans = [];
-      final parts = cleanContent.split(r'$');
-      for (int i = 0; i < parts.length; i++) {
-        if (i % 2 == 1) {
-          if (parts[i].trim().isNotEmpty) {
-            spans.add(
-              Math.tex(
-                parts[i].trim(),
-                textStyle: style ?? const TextStyle(fontSize: 13),
-                onErrorFallback: (err) => Text('\$${parts[i]}\$', style: style),
-              ),
-            );
-          }
-        } else {
-          if (parts[i].isNotEmpty) {
-            spans.add(Text(parts[i], style: style));
-          }
-        }
-      }
-      return Wrap(
-        cross: WrapCrossAlignment.center,
-        children: spans,
+    if (formattedContent.contains(r'$')) {
+      return KaTeXStyle(
+        textStyle: style ?? const TextStyle(fontSize: 13, color: Colors.black87),
+        child: KaTeX(laTeXCode: Text(formattedContent)),
       );
     }
     return Text(content, style: style);
